fa663879287786635e729544646c64050c833f09
Michael Christoffersen <mchristo28@gmail.com>
2021-02-18 19:37:50 -0700

    Big restructure

--- dem2stl.py
+++ dem2stl.py
@@ -1,211 +1,317 @@
-from osgeo import gdal
-from gdalconst import *
 import numpy as np
-import struct
-import scipy.ndimage.filters as filt
+import argparse, os, struct
+import rasterio as rio
+import scipy.signal as spsig
 
-# Michael Christoffersen
-# Python 2.7
-# Uses GDAL, numpy, scipy
-# Turns a DEM into an STL file with a base
+def genFacets(grid, dx, dy,):
+	h, w = grid.shape
+	# Elev of bottom - min minus 10pct of max-min
+	bz = grid.min() - (0.1 * (grid.max() - grid.min()))-1
+	
+	#nfacet = (w-1)*(h-1)*2
+	# Number of facets for making a volume
+	nfacet = (
+		2 * (w - 1) * (h - 1) + 4 * (w - 1) + 4 * (h - 1) + 2
+	)
 
-dem_path = './ak_crop.tif'
-out_path = './ak_crop.stl'
-dsamp = 5         # factor for downsampling the DEM
-vx = 10           # vertical exaggeration
-volume = True     # if false output is a surface, if true sides and a base are added
-base = 2000       # this is elevation below the max height to put the base, in DEM units
-smooth = True     # Whether to smooth with a gaussian kernel
-sigma = 3         # For smoothing filter
+	# Array to hold facet data
+	# Cols 1-9 hold (x1,y1,z1,x2,...,z3) of the facet corners
+	# Col 10-12 hold the normal vector to the facet
+	f = np.zeros((nfacet, 12))
 
-# Use this if the DEM has bad values stored for the pixel dimensions
-# The value should be in the same units as the DEM heights 
-force_dim = False
-dim_val = 30 
+	# Build top surfce
+	c = 0
+	for i in range(h-1):
+		for j in range(w-1):
+			## Facet 1 - upper left, bottom left, bottom right
+			# Point 1
+			f[c,3] = j*dx
+			f[c,4] = i*dy
+			f[c,5] = grid[i,j]
+			# Point 2
+			f[c,6] = j*dx
+			f[c,7] = (i+1)*dy
+			f[c,8] = grid[i+1,j]
+			# Point 3
+			f[c,9] = (j+1)*dx
+			f[c,10] = (i+1)*dy
+			f[c,11] = grid[i+1,j+1]
+			# Normal (p2-p1 X p3-p2)
+			f[c,0:3] = -1*np.cross(f[c,6:9] - f[c,3:6], f[c,9:12]-f[c,6:9])
+			c += 1
 
-### No Editing Below Here ###
+			## Facet 2 - upper left, upper right, bottom right
+			# Point 1
+			f[c,3] = j*dx
+			f[c,4] = i*dy
+			f[c,5] = grid[i,j]
+			# Point 2
+			f[c,6] = (j+1)*dx
+			f[c,7] = (i+1)*dy
+			f[c,8] = grid[i+1,j+1]
+			# Point 3
+			f[c,9] = (j+1)*dx
+			f[c,10] = i*dy
+			f[c,11] = grid[i,j+1]
+			# Normal (p2-p3 X p1-p2)
+			f[c,0:3] = np.cross(f[c,6:9] - f[c,9:12], f[c,3:6]-f[c,6:9])
+			c += 1
 
-# Cross product of two vectors
-def xprd(v1, v2):
-	i = v1[1]*v2[2] - v1[2]*v2[1]
-	j = -(v1[0]*v2[2] - v1[2]*v2[0])
-	k = v1[0]*v2[1] - v1[1]*v2[0]
-	return [i,j,k]
+	## Build sides
+	# Left/Right sides
+	j = 0
+	norm = [-1,0,0]
+	for i in range(h-1):
+		## Facet 1 - upper left, bottom left, bottom right
+		# Point 1
+		f[c,3] = j*dx
+		f[c,4] = i*dy
+		f[c,5] = grid[i,j]
+		# Point 2
+		f[c,6] = j*dx
+		f[c,7] = i*dy
+		f[c,8] = bz
+		# Point 3
+		f[c,9] = j*dx
+		f[c,10] = (i+1)*dy
+		f[c,11] = bz
+		# Normal
+		f[c,0:3] = norm
+		c += 1
 
-# Read in the DEM
-src_dem = gdal.Open(dem_path,GA_ReadOnly)
-dem_data = np.array(src_dem.GetRasterBand(1).ReadAsArray()).astype('float32')
+		## Facet 2 - upper left, upper right, bottom right
+		# Point 1
+		f[c,3] = j*dx
+		f[c,4] = i*dy
+		f[c,5] = grid[i,j]
+		# Point 3
+		f[c,6] = j*dx
+		f[c,7] = (i+1)*dy
+		f[c,8] = bz
+		# Point 2
+		f[c,9] = j*dx
+		f[c,10] = (i+1)*dy
+		f[c,11] = grid[i+1,j]
+		# Normal (p2-p3 X p1-p2)
+		f[c,0:3] = norm
+		c += 1
 
-# Make sure elevations are > 0
-if(dem_data.min() < 0):
-	dem_data = dem_data + dem_data.min() + 1
+	j = w-1
+	norm = [1,0,0]
+	for i in range(h-1):
+		## Facet 1 - upper left, bottom left, bottom right
+		# Point 1
+		f[c,3] = j*dx
+		f[c,4] = i*dy
+		f[c,5] = grid[i,j]
+		# Point 2
+		f[c,6] = j*dx
+		f[c,7] = (i+1)*dy
+		f[c,8] = bz
+		# Point 3
+		f[c,9] = j*dx
+		f[c,10] = i*dy
+		f[c,11] = bz
+		# Normal
+		f[c,0:3] = norm
+		c += 1
 
-# Subsample it and apply vertical exaggeration 
-dem_data = dem_data[::dsamp, ::dsamp]*vx
+		## Facet 2 - upper left, upper right, bottom right
+		# Point 1
+		f[c,3] = j*dx
+		f[c,4] = i*dy
+		f[c,5] = grid[i,j]
+		# Point 2
+		f[c,6] = j*dx
+		f[c,7] = (i+1)*dy
+		f[c,8] = grid[i+1,j]
+		# Point 3
+		f[c,9] = j*dx
+		f[c,10] = (i+1)*dy
+		f[c,11] = bz
+		# Normal (p2-p3 X p1-p2)
+		f[c,0:3] = norm
+		c += 1
 
-if(smooth): # Filter the dem
-	dem_data = filt.gaussian_filter(dem_data,sigma)
+	# Top/Bottom sides
+	i = 0
+	norm = [0,1,0]
+	for j in range(w-1):
+		## Facet 1 - upper left, bottom left, bottom right
+		# Point 1
+		f[c,3] = j*dx
+		f[c,4] = i*dy
+		f[c,5] = grid[i,j]
+		# Point 2
+		f[c,6] = (j+1)*dx
+		f[c,7] = i*dy
+		f[c,8] = bz
+		# Point 3
+		f[c,9] = j*dx
+		f[c,10] = i*dy
+		f[c,11] = bz
+		# Normal
+		f[c,0:3] = norm
+		c += 1
 
-# Get DEM dimensions and pixel dimensions
-y,x = dem_data.shape
-print("{} x {}".format(x,y))
-gt = src_dem.GetGeoTransform()
+		## Facet 2 - upper left, upper right, bottom right
+		# Point 1
+		f[c,3] = j*dx
+		f[c,4] = i*dy
+		f[c,5] = grid[i,j]
+		# Point 2
+		f[c,6] = (j+1)*dx
+		f[c,7] = i*dy
+		f[c,8] = grid[i,j+1]
+		# Point 3
+		f[c,9] = (j+1)*dx
+		f[c,10] = i*dy
+		f[c,11] = bz
+		# Normal (p2-p3 X p1-p2)
+		f[c,0:3] = norm
+		c += 1
 
-# This first part is for forcing pixel dimensions on poorly formed DEMs
-# Otherwise, just read the geotransform
-if(force_dim):
-	xres = dim_val * dsamp
-	yres = dim_val * dsamp
-else:
-	xres = gt[1]*dsamp
-	yres = -gt[5]*dsamp
-print("xres: {}".format(xres))
-print("yres: {}".format(yres))
+	i = h-1
+	norm = [0,-1,0]
+	for j in range(w-1):
+		## Facet 1 - upper left, bottom left, bottom right
+		# Point 1
+		f[c,3] = j*dx
+		f[c,4] = i*dy
+		f[c,5] = grid[i,j]
+		# Point 2
+		f[c,6] = j*dx
+		f[c,7] = i*dy
+		f[c,8] = bz
+		# Point 3
+		f[c,9] = (j+1)*dx
+		f[c,10] = i*dy
+		f[c,11] = bz
+		# Normal
+		f[c,0:3] = norm
+		c += 1
+
+		## Facet 2 - upper left, upper right, bottom right
+		# Point 1
+		f[c,3] = j*dx
+		f[c,4] = i*dy
+		f[c,5] = grid[i,j]
+		# Point 2
+		f[c,6] = (j+1)*dx
+		f[c,7] = i*dy
+		f[c,8] = bz
+		# Point 3
+		f[c,9] = (j+1)*dx
+		f[c,10] = i*dy
+		f[c,11] = grid[i,j+1]
+		# Normal (p2-p3 X p1-p2)
+		f[c,0:3] = norm
+		c += 1
+
+	# Add bottom
+	f[c,:] = [0,0,-1,0,0,bz,(w-1)*dx,0,bz,(w-1)*dx,(h-1)*dy,bz]
+	f[c+1,:] = [0,0,-1,0,0,bz,(w-1)*dx,(h-1)*dy,bz,0,(h-1)*dy,bz]
+
+	return f
+
+def buildSTL(grid, xres, yres, stlFile):
+	f = genFacets(grid, xres, yres)
+
+	# Start making STL file
+	fd = open(stlFile, "wb")
+
+	head = struct.pack(
+		"<" + "d" * 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	)  # 80 byte padding at beginning
+	fd.write(head)
+
+	nfac = struct.pack("<I", f.shape[0])
+	fd.write(nfac)
+
+	for i in range(f.shape[0]):
+		row = struct.pack('<'+'f'*12, f[i, 0],f[i, 1],f[i, 2],f[i, 3],f[i, 4],f[i, 5],f[i, 6],f[i, 7],f[i, 8],f[i, 9],f[i, 10],f[i, 11])
+		bc = struct.pack('<H', 0)
+		fd.write(row+bc)
+
+	fd.close()
+
+	return 0
 
 
-# Make an array of vectors along the x axis and y axis, pointing from point to adjacent point
-yxvec = np.zeros((y-1,x))
-yyvec = np.ones((y-1,x))*yres
-yzvec = np.zeros((y-1,x))
+def main():
+	# Set up CLI
+	parser = argparse.ArgumentParser(
+		description="""Program for generating a STL file from a dem
+					   requires a square or rectangular DEM with no
+					   no-data swaths. Horizontal units must match
+					   vertical units. Output filename is auto-generated
+					   if not specified.""",
+		formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+	)
+	parser.add_argument("DEM", help="Digital elevation model")
+	parser.add_argument("-o", "--output", help="Output STL file", default=None)
+	parser.add_argument(
+		"-s",
+		"--scale",
+		help="Map scale of output STL will be 1:SCALE. Assumes DEM coordinates are meters.",
+		type=float,
+		default=1,
+	)
+	parser.add_argument(
+		"-vx",
+		"--vertical-exaggeration",
+		help="Vertical exaggeration of output STL relative to input DEM",
+		type=float,
+		default=1,
+	)
+	args = parser.parse_args()
 
-xxvec = np.ones((y,x-1))*xres
-xyvec = np.zeros((y,x-1))
-xzvec = np.zeros((y,x-1))
+	# Open DEM and load data/metadata
+	ds = rio.open(args.DEM, "r")
+	grid = ds.read(1)
+	w = ds.width
+	h = ds.height
+	dx = ds.transform[0]
+	dy = ds.transform[4]
+	ds.close()
 
-for i in range(y-1):
-	np.subtract(dem_data[i+1,:],dem_data[i,:],yzvec[i,:])
+	# Generate output file name if not specified
+	if args.output == None:
+		fext = os.path.splitext(args.DEM)[1]
+		args.output = args.DEM.replace(
+			fext,
+			"_1to" + str(args.scale) + "_" + str(args.vertical_exaggeration) + "x.stl",
+		)
 
-for i in range(x-1):
-	np.subtract(dem_data[:,i+1],dem_data[:,i],xzvec[:,i])
+	# Calculate scaled dimensions and resolution
+	txdim = np.abs(dx * w)  # True dims in m
+	tydim = np.abs(dy * h)
+	sf = args.scale / 1000  # scale factor
+	sxdim = txdim / sf  # Scaled dims in mm
+	sydim = tydim / sf
+	sdx = dx / sf  # Scale dx and dy
+	sdy = dy / sf
 
-dy = np.stack((yxvec,yyvec,yzvec),0)
-dx = np.stack((xxvec,xyvec,xzvec),0)
+	# Decimate points to about 1 per mm if necessary
+	# Gets rid of extra data and noise from the stl
+	if sdx < 1:
+		xf = np.abs(int(np.ceil(1 / sdx)))
+		grid = spsig.decimate(grid, xf, axis=1, zero_phase=True)
+		sdx = sdx * xf
+		sxdim = np.abs(sdx * grid.shape[1])
+	if sdy < 1:
+		yf = np.abs(int(np.ceil(1 / sdy)))
+		grid = spsig.decimate(grid, yf, axis=0, zero_phase=True)
+		sdy = sdy * yf
+		sydim = np.abs(sdy * grid.shape[0])
 
-# Start making STL file
-outfl = open(out_path,'wb')
+	# Apply vx and scale factor to grid Z
+	grid = grid * args.vertical_exaggeration/sf
 
-head = struct.pack('<'+'d'*10,0,0,0,0,0,0,0,0,0,0) # 80 byte padding at beginning
-outfl.write(head)
+	# Make stl file
+	buildSTL(grid, sdx, sdy, args.output)
 
-# Number of facets for making a volume vs a surface
-if(volume):
-	ftot = 2*(x-1)*(y-1) + 4*(x-1) + 4*(y-1) + 2 # Total number of facets
-else:
-	ftot = (x-1)*(y-1)*2
+	return 0
 
-print("{} facets".format(ftot))
-leng = struct.pack('<I',ftot)
-outfl.write(leng)
 
-# Flip vertical to correct for top left indexing
-dem_data = np.flip(dem_data, 0)
-
-# Make the surface
-# Making triangles and calculating normal vectors
-for i in range(x-1):
-	for j in range(y-1):
-		ptl = dem_data[j,i]
-		pbl = dem_data[j+1,i]
-		ptr = dem_data[j,i+1]
-		pbr = dem_data[j+1,i+1]
-		vt = dx[:,j,i]
-		vb = dx[:,j+1,i]
-		vl = dy[:,j,i]
-		vr = dy[:,j,i+1]
-		xptl = xprd(vt,vl)
-		xpbr = xprd(vb,vr)
-		ftl = struct.pack('<' + 'f'*12 + 'H',
-				xptl[0],xptl[1],xptl[2],
-				i*xres, (j+1)*yres, pbl,
-				i*xres, j*yres, ptl,
-				(i+1)*xres, j*yres, ptr, 0)
-		fbr = struct.pack('<' + 'f'*12 + 'H',
-				xpbr[0],xpbr[1],xpbr[2],
-				(i+1)*xres, (j+1)*yres, pbr,
-				i*xres, (j+1)*yres, pbl,
-				(i+1)*xres, j*yres, ptr, 0)
-		#print(ptl,pbl,ptr,pbr)
-		outfl.write(ftl)
-		outfl.write(fbr)
-
-# Add sides and a base if making a volume
-if(volume):
-	bz = dem_data.max() - base*vx
-
-	# Add two sides
-	for i in range(x-1):
-		ptl = dem_data[0,i]
-		ptr = dem_data[0,i+1]
-		pbl = dem_data[y-1,i]
-		pbr = dem_data[y-1,i+1]
-		t1 = struct.pack('<' + 'f'*12 + 'H',
-			0,1,0,
-			i*xres, 0, ptl,
-			i*xres, 0, bz,
-			(i+1)*xres, 0, bz, 0)
-		t2 = struct.pack('<' + 'f'*12 + 'H',
-			0,1,0,
-			(i+1)*xres, 0, ptr,
-			i*xres, 0, ptl,
-			(i+1)*xres, 0, bz, 0)
-		b1 = struct.pack('<' + 'f'*12 + 'H',
-			0,-1,0,
-			i*xres, (y-1)*yres, bz,
-			i*xres, (y-1)*yres, pbl,
-			(i+1)*xres, (y-1)*yres, bz, 0)
-		b2 = struct.pack('<' + 'f'*12 + 'H',
-			0,-1,0,
-			i*xres, (y-1)*yres, pbl,
-			(i+1)*xres, (y-1)*yres, pbr,
-			(i+1)*xres, (y-1)*yres, bz, 0)
-		outfl.write(t1)
-		outfl.write(t2)
-		outfl.write(b1)
-		outfl.write(b2)
-
-	# Add the other two sides
-	for j in range(y-1):
-		ptl = dem_data[j,0]
-		ptr = dem_data[j+1,0]
-		pbl = dem_data[j,x-1]
-		pbr = dem_data[j,x-1]
-		l1 = struct.pack('<' + 'f'*12 + 'H',
-			-1,0,0,
-			0, j*yres, bz,
-			0, j*yres, ptl,
-			0, (j+1)*yres, bz, 0)
-		l2 = struct.pack('<' + 'f'*12 + 'H',
-			-1,0,0,
-			0, j*yres, ptl,
-			0, (j+1)*yres, ptr,
-			0, (j+1)*yres, bz, 0)
-		r1 = struct.pack('<' + 'f'*12 + 'H',
-			1,0,0,
-			(x-1)*xres, j*yres, pbl,
-			(x-1)*xres, j*yres, bz,
-			(x-1)*xres, (j+1)*yres, bz, 0)
-		r2 = struct.pack('<' + 'f'*12 + 'H',
-			1,0,0,
-			(x-1)*xres, (j+1)*yres, pbl,
-			(x-1)*xres, j*yres, pbr,
-			(x-1)*xres, (j+1)*yres, bz, 0)
-		outfl.write(r1)
-		outfl.write(r2)
-		outfl.write(l1)
-		outfl.write(l2)
-
-	# Add a bottom
-	f1 = struct.pack('<' + 'f'*12 + 'H',
-			0,0,-1,
-			0, 0, bz,
-			0, (y-1)*yres, bz,
-			(x-1)*xres, 0, bz, 0)
-	f2 = struct.pack('<' + 'f'*12 + 'H',
-			0,0,-1,
-			0, (y-1)*yres, bz,
-			(x-1)*xres, (y-1)*yres, bz,
-			(x-1)*xres, 0, bz, 0)
-	outfl.write(f1)
-	outfl.write(f2)
-		
-outfl.close()
+main()
