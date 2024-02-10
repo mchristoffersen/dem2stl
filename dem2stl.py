9a453c9955f488f07c0e87a7c28cebda966a1901
Michael Christoffersen <mchristo28@gmail.com>
2024-02-09 16:21:10 -0900

    Added pyproject, auto-formatted, updated readme

--- dem2stl.py
+++ dem2stl.py
@@ -5,111 +5,122 @@ import scipy.signal as spsig
 import matplotlib.pyplot as plt
 
 
-def genFacets(grid, dx, dy, bpct=.1):
+def genFacets(grid, dx, dy, bpct=0.2):
     h, w = grid.shape
     # Elev of bottom - min minus 10pct of max-min
     bz = grid.min() - (bpct * (grid.max() - grid.min()))
 
-    #nfacet = (w-1)*(h-1)*2
+    # nfacet = (w-1)*(h-1)*2
     # Number of facets for making a volume
-    nfacet = (
-        2 * (w - 1) * (h - 1) + 4 * (w - 1) + 4 * (h - 1) + 2 + 6
-    )
+    nfacet = 2 * (w - 1) * (h - 1) + 4 * (w - 1) + 4 * (h - 1) + 2 + 6
 
     # Array to hold facet data
     # Cols 1-9 hold (x1,y1,z1,x2,...,z3) of the facet corners
     # Col 10-12 hold the normal vector to the facet
-    f = np.zeros((nfacet, 12)) # all facets
+    f = np.zeros((nfacet, 12))  # all facets
 
     # Build top surface
     c = 0
-    for i in range(h-1):
-        for j in range(w-1):
+    for i in range(h - 1):
+        for j in range(w - 1):
             ## Facet 1 - upper left, bottom left, bottom right
             # Point 1
-            f[c,3] = j*dx
-            f[c,4] = i*dy
-            f[c,5] = grid[i,j]
+            f[c, 3] = j * dx
+            f[c, 4] = i * dy
+            f[c, 5] = grid[i, j]
             # Point 2
-            f[c,6] = j*dx
-            f[c,7] = (i+1)*dy
-            f[c,8] = grid[i+1,j]
+            f[c, 6] = j * dx
+            f[c, 7] = (i + 1) * dy
+            f[c, 8] = grid[i + 1, j]
             # Point 3
-            f[c,9] = (j+1)*dx
-            f[c,10] = (i+1)*dy
-            f[c,11] = grid[i+1,j+1]
+            f[c, 9] = (j + 1) * dx
+            f[c, 10] = (i + 1) * dy
+            f[c, 11] = grid[i + 1, j + 1]
             # Normal (p2-p1 X p3-p2)
-            f[c,0:3] = -1*np.cross(f[c,6:9] - f[c,3:6], f[c,9:12]-f[c,6:9])
+            f[c, 0:3] = -1 * np.cross(f[c, 6:9] - f[c, 3:6], f[c, 9:12] - f[c, 6:9])
             c += 1
 
             ## Facet 2 - upper left, upper right, bottom right
             # Point 1
-            f[c,3] = j*dx
-            f[c,4] = i*dy
-            f[c,5] = grid[i,j]
+            f[c, 3] = j * dx
+            f[c, 4] = i * dy
+            f[c, 5] = grid[i, j]
             # Point 2
-            f[c,6] = (j+1)*dx
-            f[c,7] = (i+1)*dy
-            f[c,8] = grid[i+1,j+1]
+            f[c, 6] = (j + 1) * dx
+            f[c, 7] = (i + 1) * dy
+            f[c, 8] = grid[i + 1, j + 1]
             # Point 3
-            f[c,9] = (j+1)*dx
-            f[c,10] = i*dy
-            f[c,11] = grid[i,j+1]
+            f[c, 9] = (j + 1) * dx
+            f[c, 10] = i * dy
+            f[c, 11] = grid[i, j + 1]
             # Normal (p3-p2 X p1-p3)
-            f[c,0:3] = np.cross(f[c,9:12] - f[c,6:9], f[c,3:6]-f[c,9:12])
+            f[c, 0:3] = np.cross(f[c, 9:12] - f[c, 6:9], f[c, 3:6] - f[c, 9:12])
             c += 1
 
     ## List points around the DEM border and use to build sides
-    b = np.zeros((2*w+2*h, 3)) # x,y,z for border points
+    b = np.zeros((2 * w + 2 * h, 3))  # x,y,z for border points
     # Top edge
-    b[0:w, 0] = np.arange(w)*dx
+    b[0:w, 0] = np.arange(w) * dx
     b[0:w, 1] = 0
-    b[0:w, 2] = grid[0,:]
+    b[0:w, 2] = grid[0, :]
     # Right edge
-    b[w:w+h, 0] = (w-1)*dx
-    b[w:w+h, 1] = np.arange(h)*dy
-    b[w:w+h, 2] = grid[:,w-1]
+    b[w : w + h, 0] = (w - 1) * dx
+    b[w : w + h, 1] = np.arange(h) * dy
+    b[w : w + h, 2] = grid[:, w - 1]
     # Bottom edge
-    b[w+h:2*w+h, 0] = np.flip(np.arange(w)*dx)
-    b[w+h:2*w+h, 1] = (h-1)*dy
-    b[w+h:2*w+h, 2] = np.flip(grid[h-1,:])
+    b[w + h : 2 * w + h, 0] = np.flip(np.arange(w) * dx)
+    b[w + h : 2 * w + h, 1] = (h - 1) * dy
+    b[w + h : 2 * w + h, 2] = np.flip(grid[h - 1, :])
     # Left edge
-    b[2*w+h:2*w+2*h, 0] = 0
-    b[2*w+h:2*w+2*h, 1] = np.flip(np.arange(h)*dy)
-    b[2*w+h:2*w+2*h, 2] = np.flip(grid[:,0])
+    b[2 * w + h : 2 * w + 2 * h, 0] = 0
+    b[2 * w + h : 2 * w + 2 * h, 1] = np.flip(np.arange(h) * dy)
+    b[2 * w + h : 2 * w + 2 * h, 2] = np.flip(grid[:, 0])
 
-    for i in range(len(b)-1):
+    for i in range(len(b) - 1):
         ## Facet 1 - upper left, bottom left, bottom right
         # Point 1
-        f[c,3:6] = b[i]
+        f[c, 3:6] = b[i]
         # Point 2
-        f[c,6] = b[i+1][0]
-        f[c,7] = b[i+1][1]
-        f[c,8] = bz
+        f[c, 6] = b[i + 1][0]
+        f[c, 7] = b[i + 1][1]
+        f[c, 8] = bz
         # Point 3
-        f[c,9] = b[i][0]
-        f[c,10] = b[i][1]
-        f[c,11] = bz
+        f[c, 9] = b[i][0]
+        f[c, 10] = b[i][1]
+        f[c, 11] = bz
         # Normal (p3-p2 X p1-p3)
-        f[c,0:3] = -1*np.cross(f[c,9:12] - f[c,6:9], f[c,3:6]-f[c,9:12])
+        f[c, 0:3] = -1 * np.cross(f[c, 9:12] - f[c, 6:9], f[c, 3:6] - f[c, 9:12])
         c += 1
 
         ## Facet 2 - upper left, upper right, bottom right
         # Point 1
-        f[c,3] = b[i+1][0]
-        f[c,4] = b[i+1][1]
-        f[c,5] = bz
+        f[c, 3] = b[i + 1][0]
+        f[c, 4] = b[i + 1][1]
+        f[c, 5] = bz
         # Point 2
-        f[c,6:9] = b[i]
+        f[c, 6:9] = b[i]
         # Point 3
-        f[c,9:12] = b[i+1]
+        f[c, 9:12] = b[i + 1]
         # Normal (p3-p2 X p1-p3)
-        f[c,0:3] = -1*np.cross(f[c,9:12] - f[c,6:9], f[c,3:6]-f[c,9:12])
+        f[c, 0:3] = -1 * np.cross(f[c, 9:12] - f[c, 6:9], f[c, 3:6] - f[c, 9:12])
         c += 1
 
     # Add bottom
-    f[c,:] = [0,0,-1,0,0,bz,(w-1)*dx,0,bz,(w-1)*dx,(h-1)*dy,bz]
-    f[c+1,:] = [0,0,-1,0,0,bz,(w-1)*dx,(h-1)*dy,bz,0,(h-1)*dy,bz]
+    f[c, :] = [0, 0, -1, 0, 0, bz, (w - 1) * dx, 0, bz, (w - 1) * dx, (h - 1) * dy, bz]
+    f[c + 1, :] = [
+        0,
+        0,
+        -1,
+        0,
+        0,
+        bz,
+        (w - 1) * dx,
+        (h - 1) * dy,
+        bz,
+        0,
+        (h - 1) * dy,
+        bz,
+    ]
 
     return f
 
@@ -129,9 +140,23 @@ def buildSTL(grid, xres, yres, stlFile):
     fd.write(nfac)
 
     for i in range(f.shape[0]):
-        row = struct.pack('<'+'f'*12, f[i, 0],f[i, 1],f[i, 2],f[i, 3],f[i, 4],f[i, 5],f[i, 6],f[i, 7],f[i, 8],f[i, 9],f[i, 10],f[i, 11])
-        bc = struct.pack('<H', 0)
-        fd.write(row+bc)
+        row = struct.pack(
+            "<" + "f" * 12,
+            f[i, 0],
+            f[i, 1],
+            f[i, 2],
+            f[i, 3],
+            f[i, 4],
+            f[i, 5],
+            f[i, 6],
+            f[i, 7],
+            f[i, 8],
+            f[i, 9],
+            f[i, 10],
+            f[i, 11],
+        )
+        bc = struct.pack("<H", 0)
+        fd.write(row + bc)
 
     fd.close()
 
@@ -146,27 +171,27 @@ def factorize(x):
     while x > 8:
         if not x % f:
             factors.append(f)
-            x = x/f
+            x = x / f
             f = 2
         else:
             f += 1
-            if(f == x):
+            if f == x:
                 print("Factorization failure - probably prime.")
                 print("x = %d" % x)
                 print("Subtracting 1 and trying again")
                 x -= 1
                 f = 2
     factors.append(int(x))
-    
+
     # Check that factorization worked
-    if(np.product(factors) != x0):
+    if np.product(factors) != x0:
         print("Factorization failure - product of factors is not original number")
         print("Product of factors: %d\nOriginal Number: %d" % (np.product(factors), x0))
-        
+
     return factors
 
 
-def main():
+def cli():
     # Set up CLI
     parser = argparse.ArgumentParser(
         description="""Program for generating a STL file from a dem
@@ -200,7 +225,11 @@ def main():
         default=1,
     )
 
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def main():
+    args = cli()
 
     print("Creating STL file from " + args.DEM)
     # Open DEM and load data/metadata
@@ -223,36 +252,37 @@ def main():
 
     # Decimate points to about point-density points per mm
     if sdx < 1:
-        xf = np.abs(int(1/args.point_density/sdx))
+        xf = np.abs(int(1 / args.point_density / sdx))
         xfs = factorize(xf)
         for f in xfs:
-            grid = spsig.decimate(grid, f, axis=1, zero_phase=True, ftype="iir")     
+            grid = spsig.decimate(grid, f, axis=1, zero_phase=True, ftype="iir")
         sdx = sdx * xf
         sxdim = np.abs(sdx * grid.shape[1])
     if sdy < 1:
-        yf = np.abs(int(1/args.point_density/sdy))
+        yf = np.abs(int(1 / args.point_density / sdy))
         yfs = factorize(yf)
         for f in yfs:
-            grid = spsig.decimate(grid, f, axis=0, zero_phase=True, ftype="iir")             
+            grid = spsig.decimate(grid, f, axis=0, zero_phase=True, ftype="iir")
         sdy = sdy * yf
         sydim = np.abs(sdy * grid.shape[0])
 
     # Trim grid to get rid of edge effects
-    grid = grid 
+    grid = grid
 
     # Generate output file name if not specified
     if args.output == None:
         fext = os.path.splitext(args.DEM)[1]
         args.output = args.DEM.replace(
             fext,
-            "_1to%dk_%dx_%.2fppmm.stl" % (int(args.scale/1000), args.vertical_exaggeration, 1/sdx),
+            "_1to%dk_%.1fx_%.2fppmm.stl"
+            % (int(args.scale / 1000), args.vertical_exaggeration, 1 / sdx),
         )
     print("STL will be saved to " + args.output)
     print("STL dimensions: %.0fmm X %.0fmm" % (sxdim, sydim))
-    print("STL point density: %.2f point(s)/mm" % (1/sdx))
+    print("STL point density: %.2f point(s)/mm" % (1 / sdx))
 
     # Apply vx and scale factor to grid Z
-    grid = grid * args.vertical_exaggeration/sf
+    grid = grid * args.vertical_exaggeration / sf
 
     # Make stl file
     buildSTL(grid, sdx, sdy, args.output)
@@ -260,4 +290,5 @@ def main():
     return 0
 
 
-main()
+if __name__ == "__main__":
+    main()
--- readme.md
+++ readme.md
@@ -1,6 +1,12 @@
 # dem2stl
-dem2stl is a python script to transform a square or rectangular DEM into a 3D print-able STL file. Argparse printout explaining usage -  
-
+dem2stl is a python script to transform a square or rectangular DEM into a 3D print-able STL file.  
+  
+dem2stl can be installed with pip:  
+pip install git+https://github.com/mchristoffersen/dem2stl.git  
+  
+dem2stl can't handle DEMs with no-data cells. Also make sure that the elevation units are the same as the map projection units. Mixed unit DEMs (e.g. x/y in decimal degrees and z in meters) won't work well.   
+  
+Argparse printout explaining usage -  
 ```
 usage: dem2stl.py [-h] [-o OUTPUT] [-s SCALE] [-vx VERTICAL_EXAGGERATION]  
                   [-p POINT_DENSITY]  
